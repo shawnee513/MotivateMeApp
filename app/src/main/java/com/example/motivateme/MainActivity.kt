83aadb49deef862a6f4d1779ae25e8811a31ad32
shawnee <shanpan513@gmail.com>
2022-07-07 14:24:02 -0600

    added functionality to get name from user and update display message

--- app/src/main/java/com/example/motivateme/MainActivity.kt
+++ app/src/main/java/com/example/motivateme/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.motivateme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     //override lifecylce method onCreate. This should initialize all important items for the code.
@@ -10,5 +14,21 @@ class MainActivity : AppCompatActivity() {
 
         //inflate the given layout to turn xml views into kotlin objects.
         setContentView(R.layout.activity_main)
+
+        //Get a reference to the button view from our layout and st a clickListener
+        val updateButton: Button = findViewById(R.id.activity_main_bt_update)
+        updateButton.setOnClickListener { updateMessage() }
+    }
+
+    //Get a name from an edit text and display a personalized message
+    private fun updateMessage(){
+        val name: EditText = findViewById(R.id.activity_main_et_name)
+        val message: TextView = findViewById(R.id.main_activity_tx_message)
+
+        //Get the username from the editText
+        val username = name.text
+
+        //Update the textView to display our message
+        message.text = "Hello $username!"
     }
 }
